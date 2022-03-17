f69aa5fa892269f0fd6bfb2b7906ad6a5e843de8
JuanRodriguez95 <juanrodriguezcampos818@gmail.com>
2022-03-17 08:44:09 -0500

    Modelo, controllador y vista de Book; Campo nombre validado

---
+++ BiblioNetAPP/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BiblioNetAPP.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="{0} is required")]
+        public string BookName { get; set; }
+        public Author Author { get; set; }
+        public double price { get; set; }
+    }
+}
